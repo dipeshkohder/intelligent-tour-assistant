48c0db682e22adf1df177fbd546614226cc731bf
dhairyaHere <dhairya2308@gmail.com>
2020-03-17 19:33:31 +0530

    BCOZ THESE WAS ALL UNCOMMITED IN MY PROJECT FOLDER

--- planning/urls.py
+++ planning/urls.py
@@ -1,12 +1,16 @@
 
 from django.urls import path
-from planning.views import trip,homepage,distance,travellingSalesmanProblem,next_permutation,deletefunction
+from planning.views import login,adduser_info,trip,homepage,distance,travellingSalesmanProblem,next_permutation,deletefunction,auth_view,signup
 from . import views
 from django.conf.urls import url
 urlpatterns = [
  path('', views.index, name='index'),
  url(r'^trip/$', trip),
  url(r'^home/$', homepage),
- url(r'^trip/planning/deletefunction/$',deletefunction)
+ url(r'^trip/planning/deletefunction/$',deletefunction),
+ url(r'^login/$',login),
+ url(r'^checklogin/$',auth_view),
+ url(r'^signup/$',signup),
+ url(r'^adduser/$',adduser_info),
  #url(r'^show/$',show),
 ]
